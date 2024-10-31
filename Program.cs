0df5e6b5220726ccb6b930107b42ef7d017d3804
HoStWgSp <hostwg@vk.com>
2024-10-31 16:38:43 +1100

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace CoreStartApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Выводим информационное сообщение 
+            //PrintMessage((() => Info("Запускаем приложение")));
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+    
+}
---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿namespace CoreStartApp
+{
+    public class Startup
+    {
+        // Метод вызывается средой ASP.NET.
+        // Используйте его для подключения сервисов приложения
+        // Документация:  https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // Метод вызывается средой ASP.NET.
+        // Используйте его для настройки конвейера запросов
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment() || env.IsStaging())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // Добавим в конвейер запросов обработчик самым простым способом
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync($"Welcome to the {env.ApplicationName}!");
+            });
+        }
+    }
+}
